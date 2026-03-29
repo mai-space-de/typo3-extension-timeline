44b5a535ab14729821fac038bf33daf74ae0df23
maispace <joel@maispace.de>
2026-03-29 16:20:47 +0200

    [TASK] Apply bugfixes for use in conjunction with other extensions

---
+++ Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'ext-maispace-mai_timeline' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:mai_timeline/Resources/Public/Icons/Extension.svg',
+    ],
+];
